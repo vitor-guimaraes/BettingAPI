96ea6aa1d5890545bc7f72dde8a1e34b7ce0af6f
Vitor Guimarães <guimvitor@gmail.com>
2022-09-15 21:03:30 -0300

    Domain and Infrastructure layers added

---
+++ BettingAPI/Models/Championships.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BettingAPI.Models
+{
+    public class Championships
+    {
+        [JsonProperty("campeonato_id")]
+        public int CampeonatoId { get; set; }
+
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+
+        [JsonProperty("nome_popular")]
+        public string NomePopular { get; set; }
+
+        [JsonProperty("edicao_atual")]
+        public EdicaoAtual EdicaoAtual { get; set; }
+
+        [JsonProperty("fase_atual")]
+        public FaseAtual FaseAtual { get; set; }
+
+        [JsonProperty("rodada_atual")]
+        public int RodadaAtual { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("tipo")]
+        public string Tipo { get; set; }
+
+        [JsonProperty("logo")]
+        public string Logo { get; set; }
+
+        [JsonProperty("_link")]
+        public string Link { get; set; }
+
+    }
+}
+
+    public class FaseAtual
+        {
+            [JsonProperty("fase_id")]
+            public int FaseId { get; set; }
+            [JsonProperty("nome")]
+            public string Nome { get; set; }
+
+            [JsonProperty("slug")]
+            public string Slug { get; set; }
+            [JsonProperty("tipo")]
+            public string Tipo { get; set; }
+            [JsonProperty("_link")]
+            public string Link { get; set; }
+        }   
+
+
+    public class EdicaoAtual
+        {
+            [JsonProperty("edicao_id")]
+            public int EdicaoId { get; set; }
+            [JsonProperty("temporada")]
+            public int Temporada { get; set; }
+            [JsonProperty("nome")]
+            public string Nome { get; set; }
+            [JsonProperty("nome_popular")]
+            public string NomePopular { get; set; }
+            [JsonProperty("slug")]
+            public string Slug { get; set; }
+        }
+
+
